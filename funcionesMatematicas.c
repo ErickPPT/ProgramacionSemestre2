67aadf672255629764e50e1db05643b8e9931af3
ErickPPT <paul.pisuna2006@gmail.com>
2025-04-30 14:08:53 -0500

    Update funcionesMatematicas.c

--- funcionesMatematicas.c
+++ funcionesMatematicas.c
@@ -3,30 +3,29 @@ void suma();
 void resta(int a, int b);
 int multiplicacion();
 int division(int, int);
+int leerNumero(char*);
 
 int main(int argc, char const *argv[]){
     int num1, num2, resultado;
     printf("*******SUMA******\n");
     suma();
     printf("*******RESTA******\n");
-    printf("Ingresa un numero: ");
-    scanf("%d", &num1);
-    printf("Ingresa otro numero: ");
-    scanf("%d", &num2);
+    num1 = leerNumero("Ingres un operando: ");
+    num2 = leerNumero("Ingrese un operando: ");
     resta(num1, num2);
     printf("*******DIVISION******\n");
+    num1 = leerNumero("Ingrese un operando: ");
+    num2 = leerNumero("Ingrese un operando: ");
     printf("%d / %d = %d\n", num1, num2, division(num1, num2));
-    resultado = multiplicacion();
     printf("*******MULTIPLICACION******\n");
+    resultado = multiplicacion();
     printf("La variable resultado tiene %d\n", resultado);
     return 0;
 }
  void suma(){
     int a, b;
-    printf("Ingresa un numero: ");
-    scanf("%d", &a);
-    printf("Ingresa otro numero: ");
-    scanf("%d", &b);
+      a = leerNumero("Ingrese un numero: ");
+      b = leerNumero("Ingrese un numero: ");
     printf("La suma de %d y %d es: %d\n", a, b, (a+b));
  }
 
@@ -35,12 +34,22 @@ int main(int argc, char const *argv[]){
  }
  int multiplicacion(){
     int a, b;
-    printf("Ingresa un numero:");
-    scanf("%d", &a);
-    printf("Ingresa otro numero:");
-    scanf("%d", &b);
+    a = leerNumero("Ingrese un numero: ");
+    b = leerNumero("Ingrese un numero: ");
     return a*b;
  }
 int division(int a, int b){
     return a/b;
+ }
+ int leerNumero(char* mensaje){
+   int valor;
+   printf("%s: ", mensaje);
+   while (scanf("%d", &valor)!=1)
+   {
+      printf("Dato mal ingresado\n");
+      while(getchar()!='\n');
+      printf("%s: ", mensaje);
+   }
+   return valor;
+   
  }
